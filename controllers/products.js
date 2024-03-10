ac9c9dfafebc5347fecb720c206b25b509db2b2f
Nirzon Karmakar <nirzon47@gmail.com>
2024-03-11 00:54:12 +0530

    feat: controller for adding products

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,28 @@
+import ProductsModel from '../models/products.js'
+
 const addProduct = async (req, res) => {
-	res.send('dummy')
+	try {
+		// Adds the form data and file path to the product object
+		const product = { ...req.body, imagePath: req.file.path }
+
+		// Creates a new product model and saves it to the database
+		const newProduct = new ProductsModel(product)
+		await newProduct.save()
+
+		// Upon success, send a success response
+		res.status(200).json({
+			success: true,
+			message: 'Product created successfully',
+			product: newProduct,
+		})
+	} catch (error) {
+		// Upon failure, send an error response
+		res.status(500).json({
+			success: false,
+			message: 'Product creation failed',
+			error,
+		})
+	}
 }
 
 const productsController = { addProduct }
