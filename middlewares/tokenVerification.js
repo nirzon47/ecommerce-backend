8c1c86cbdae3ea3e8e25d71c25e7e5d18e5f425a
Nirzon Karmakar <nirzon47@gmail.com>
2024-03-11 19:49:13 +0530

    feat: middleware to verify token validity

---
+++ middlewares/tokenVerification.js
@@ -0,0 +1,34 @@
+import { userModel } from '../models/users.js'
+import jwt from 'jsonwebtoken'
+
+const tokenVerification = (role) => async (req, res, next) => {
+	try {
+		// Token from header comes in the format of 'Bearer <token>'
+		const tokenFromHeader = req.headers.token.split(' ')[1]
+		// Decodes the token
+		const token = jwt.decode(tokenFromHeader)
+		// Verifies the token
+		const isVerified = jwt.verify(tokenFromHeader, process.env.JWT_SECRET)
+		// Checks if the user is authorized
+		const isAuthorized = role.includes(token.role)
+
+		// Checks if the token is valid from database level
+		const user = await userModel.findById(token._id)
+		const checkFromDB = user.tokens.includes(tokenFromHeader)
+
+		// If the token is invalid, send an error response
+		if (!token || !isVerified || !isAuthorized || !checkFromDB) {
+			throw new Error('Forbidden')
+		}
+
+		// If the token is valid, continues to the next middleware
+		next()
+	} catch (error) {
+		return res.status(403).json({
+			success: false,
+			message: 'Forbidden',
+		})
+	}
+}
+
+export default tokenVerification
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 import productsController from '../controllers/products.js'
 import upload from '../middlewares/productsMulterConfig.js'
+import tokenVerification from '../middlewares/tokenVerification.js'
 
 const router = Router()
 
-router.post('/', upload.single('productImage'), productsController.addProduct)
+router.post(
+	'/',
+	[upload.single('productImage'), tokenVerification],
+	productsController.addProduct
+)
 
 export default router
