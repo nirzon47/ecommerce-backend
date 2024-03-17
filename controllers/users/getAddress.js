0275b60386e1778b57346ec0bb8c7e498306dc24
Nirzon Karmakar <nirzon47@gmail.com>
2024-03-18 00:23:28 +0530

    docs: added comments

--- controllers/users/getAddress.js
+++ controllers/users/getAddress.js
@@ -1,17 +1,22 @@
 import { userModel } from '../../models/users.js'
 
+// Controller for getting address
 export const getAddress = async (req, res) => {
 	try {
+		// Get the user ID from the token middleware
 		const userID = req.user._id
 
+		// Get only address from the user
 		const user = await userModel.findById(userID, {
 			address: 1,
 		})
 
+		// If the user has no address, send an error response
 		if ('{}' === JSON.stringify(user.address)) {
 			throw new Error('No address')
 		}
 
+		// Upon success, send a success response
 		res.status(200).json({
 			success: true,
 			message: 'Get address successfully',
