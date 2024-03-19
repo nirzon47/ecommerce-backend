756205759d97209d7033ff506776c8d6a0276987
Nirzon Karmakar <nirzon47@gmail.com>
2024-03-19 23:18:34 +0530

    fix: total calculation

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -3,24 +3,26 @@ import dayjs from 'dayjs'
 
 export const checkout = async (req, res) => {
 	try {
+		// Gets the user ID from the middleware
 		const userID = req.user._id
 
-		const cart = await cartModel.findOne({ user: userID }).populate([
-			{
-				path: 'products.item',
-				select: 'name price imagePath description',
-			},
-			{ path: 'user', select: 'firstName lastName address' },
-		])
+		// Gets the cart of the user and populates the item and user fields
+		const cart = await cartModel.findOne({ user: userID }).populate({
+			path: 'products.item',
+			select: 'name price',
+		})
 
+		// Calculates the total price of the cart
 		const total = cart.products.reduce(
-			(sum, item) => sum + item.quantity * item.price,
+			(sum, cartItem) => sum + cartItem.quantity * cartItem.item.price,
 			0
 		)
 
+		// Generates a random delivery date
 		const randomNumber = Math.floor(Math.random() * 7)
 		const deliveryDate = dayjs().add(randomNumber, 'days')
 
+		// Creates the order
 		const order = {
 			user: userID,
 			cart,
