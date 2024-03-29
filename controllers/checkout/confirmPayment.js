c4bba9cddf82909ba76a6f40059243305229ad8b
Nirzon Karmakar <nirzon47@gmail.com>
2024-03-29 23:29:38 +0530

    feat: confirm payment route

---
+++ controllers/checkout/confirmPayment.js
@@ -0,0 +1,26 @@
+import orderHistoryModel from '../../models/orders.js'
+
+// Controller for confirming payment
+export const confirmPayment = async (req, res) => {
+	try {
+		// Gets the order ID from the request body
+		const orderID = req.body.orderID
+
+		const updatedOrder = await orderHistoryModel.findOneAndUpdate(
+			{ 'orders.cart._id': orderID },
+			{ $set: { 'orders.$.paymentStatus': 'Paid' } }
+		)
+
+		res.json({
+			success: true,
+			message: 'Payment successful',
+			order: updatedOrder,
+		})
+	} catch (error) {
+		// Upon failure, send an error response
+		res.status(500).json({
+			success: false,
+			message: error.message || 'Payment failed',
+		})
+	}
+}
