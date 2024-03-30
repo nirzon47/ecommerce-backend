147be144193f3502385145ce1b236dd1a7491c3b
Nirzon Karmakar <nirzon47@gmail.com>
2024-03-30 22:37:24 +0530

    feat: reset password link

--- controllers/forgot.js
+++ controllers/forgot.js
@@ -45,3 +45,54 @@ export const forgotPassword = async (req, res) => {
 		})
 	}
 }
+
+// Controller for reset password
+
+export const resetPassword = async (req, res) => {
+	try {
+		// Gets the token from the request params
+		const token = req.params.token
+		// Gets the password from the request body
+		const { password } = req.body
+
+		// Checks if the token exists
+		const checkToken = await tokenModel.findOne({ token })
+
+		// If the token does not exist, send an error response
+		if (!checkToken) {
+			return res.status(404).json({
+				success: false,
+				message: 'Token not found',
+			})
+		}
+
+		// If the token has expired, send an error response
+		if (checkToken.expiry < Date.now()) {
+			return res.status(404).json({
+				success: false,
+				message: 'Token expired',
+			})
+		}
+
+		// If the token is valid, update the password
+		const user = await userModel.findById(checkToken.user)
+
+		user.password = password
+		await user.save()
+
+		// Delete the token
+		await tokenModel.findByIdAndDelete(checkToken._id)
+
+		// Upon success, send a success response
+		res.status(200).json({
+			success: true,
+			message: 'Password reset successful',
+		})
+	} catch (error) {
+		// Upon failure, send an error response
+		res.status(500).json({
+			success: false,
+			message: error.message || 'Reset password failed',
+		})
+	}
+}
--- routes/forgot.js
+++ routes/forgot.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { forgotPassword } from '../controllers/forgot.js'
+import { resetPassword } from '../controllers/forgot.js'
 
 const router = Router()
 
 router.post('/', forgotPassword)
+
+router.post('/:token', resetPassword)
 
 export default router
